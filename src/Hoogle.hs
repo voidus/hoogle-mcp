e7a5129e3af04b769ef8128db2f5c24b2f2d536e
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2026-01-10 21:31:09 +0100

    hopefully improve guidance

--- src/Hoogle.hs
+++ src/Hoogle.hs
@@ -7,16 +7,26 @@ import System.Process (readProcess)
 
 -- | Hoogle search tools for querying Haskell documentation.
 --
--- Hoogle supports several query types:
+-- CRITICAL: Hoogle is NOT like Google. Do NOT use natural language queries!
+-- BAD: \"how to parse JSON\", \"convert string to int\", \"read a file\"
+-- GOOD: \"parseJSON\", \"Text -> Int\", \"FilePath -> IO ByteString\"
+--
+-- Common use cases:
+-- - Look up a function's type: just use the function name (\"foldl\", \"traverse\")
+-- - Find functions by type: use type signatures (\"[a] -> Int\", \"Maybe a -> a\")
+-- - Find similar functions: search by partial type (\"a -> Maybe b\")
+--
+-- Hoogle supports three query types:
 --
 -- 1. NAME SEARCH: Search for a function, type, or module by name
---    Examples: \"map\", \"foldr\", \"Maybe\", \"Data.List\"
+--    Examples: \"map\", \"foldr\", \"Maybe\", \"Data.List\", \"parseJSON\"
+--    Use this to look up a specific function's type signature and documentation
 --
 -- 2. TYPE SEARCH: Search by type signature using Haskell syntax
---    Examples: \"(a -> b) -> [a] -> [b]\", \"a -> Maybe a\", \"Int -> String\"
+--    Examples: \"(a -> b) -> [a] -> [b]\", \"Text -> ByteString\", \"FilePath -> IO String\"
 --
 -- 3. PACKAGE-QUALIFIED SEARCH: Limit search to a specific package using \"+package\"
---    Examples: \"+base map\", \"+text concat\", \"+optparse-applicative many\"
+--    Examples: \"+base map\", \"+text concat\", \"+aeson parseJSON\"
 --
 -- IMPORTANT BEST PRACTICES:
 -- - Make ONE focused query at a time
